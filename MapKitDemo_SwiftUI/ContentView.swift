7be592172c52f9a4ff0dca39b110aeba6ca21842
Holger Hinzberg <holger@hinzberg.de>
2019-12-15 10:02:55 +0100

    Initial Commit

---
+++ MapKitDemo_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapKitDemo_SwiftUI
+//
+//  Created by Holger Hinzberg on 15.12.19.
+//  Copyright © 2019 Holger Hinzberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
